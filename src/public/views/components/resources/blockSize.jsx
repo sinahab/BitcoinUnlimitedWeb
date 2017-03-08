6f2f24e1f85483d99b10eeb4d08210070fe49f86
Sina Habibian <sinahab@gmail.com>
2017-03-07 19:11:46 -0800

    Add misc links

--- src/public/views/components/resources/blockSize.jsx
+++ src/public/views/components/resources/blockSize.jsx
@@ -9,7 +9,7 @@ export default React.createClass({
     render: function () {
         return (
             <div>
-                <p>This chart shows the percentage of the network that will reject a block of a given size. The <a className="link--underline black dim" href="#">emergent block size</a> limit is defined as the greatest block size that no less than half of the hash power will accept.</p>
+                <p>This chart shows the percentage of the network that will reject a block of a given size. The <a className="link--underline black dim" target='_blank' href="/resources/feemarket.pdf">emergent block size</a> limit is defined as the greatest block size that no less than half of the hash power will accept.</p>
                 <div className='center p1'>
                     <img className='section__image' src="../img/resources/block-size.png" alt="Emergent Block Size Limit" />
                 </div>
--- src/public/views/pages/technologies.jsx
+++ src/public/views/pages/technologies.jsx
@@ -60,7 +60,7 @@ class Technologies extends React.Component {
                     <img className="section__image" src="../img/xthin.png" alt="Xthin" />
                 </div>
                 <p>
-                    Test results validated that Xthin improved block propagation times by a factor of 5.6x across the normal P2P network, by a factor of 8.7x across the Great Firewall of China, while reducing the number of bytes required by a factor of 24x. For further information, please refer to our <a className='black dim link--underline' href="#">five-part Xthin article series</a>.
+                    Test results validated that Xthin improved block propagation times by a factor of 5.6x across the normal P2P network, by a factor of 8.7x across the Great Firewall of China, while reducing the number of bytes required by a factor of 24x. For further information, please refer to our <a className='black dim link--underline' target='_blank' href="https://medium.com/@peter_r/towards-massive-on-chain-scaling-presenting-our-block-propagation-results-with-xthin-da54e55dc0e4#.ul0jjmv4z">five-part Xthin article series</a>.
                 </p>
             </div>
 
