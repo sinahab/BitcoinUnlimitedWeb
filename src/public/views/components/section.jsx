84d3decd4e6f37ab5f01db18d92801c1bd932a52
Sina Habibian <sinahab@gmail.com>
2017-03-18 11:58:17 -0700

    Modify Section component so that clicking on the arrow or title expands it

--- src/public/views/components/section.jsx
+++ src/public/views/components/section.jsx
@@ -27,11 +27,9 @@ class Section extends React.Component {
     render() {
         return (
             <div className='pt1 pb2'>
-                <div className='inline pr1'>
-                    <ArrowIcon onClick={this.toggleExpanded} expanded={this.state.expanded} />
-                </div>
-                <div className="inline bold">
-                    { this.props.title }
+                <div onClick={this.toggleExpanded}>
+                    <div className='inline pr1'> <ArrowIcon expanded={this.state.expanded} /> </div>
+                    <div className='inline bold'> { this.props.title } </div>
                 </div>
                 <div className={'lh-copy pl2 ' + this.getExpandedClass()}>{ this.props.body }</div>
             </div>
