a11f837e8d04c2bf3474236f844db2b2b6e7a4c6
Sina Habibian <sinahab@gmail.com>
2017-04-04 10:30:37 -0700

    Update site footer to reflect the content in the site header

--- src/public/views/footer.jsx
+++ src/public/views/footer.jsx
@@ -12,32 +12,45 @@ class Footer extends React.Component {
                 <div className='footer__container white'>
                     <div className="px2 pb1 center">
                         <div className='left-align align-top inline-block footer__section'>
-                            <h4>{ strings().footer.news }</h4>
-                            <a className='block py1 link dim' target='_blank' href='http://gavinandresen.ninja/time-to-roll-out-bigger-blocks'>{ strings().footer.time }</a>
-                            <a className='block py1 link dim' target='_blank' href='https://medium.com/faith-and-future/why-is-bitcoin-forking-d647312d22c1'>{ strings().footer.why }</a>
-                            <a className='block py1 link dim' target='_blank' href='https://medium.com/@octskyward/on-consensus-and-forks-c6a050c792e7#.s7d93q1a9'>{ strings().footer.consensus }</a>
-                            <a className='block py1 link dim' target='_blank' href='http://gavinandresen.ninja/designing-for-success'>{ strings().footer.designing }</a>
+                            <h4><Link className='white text-decoration-none' to='/solutions'>{strings().header.solutions.title}</Link></h4>
+                            <Link className='block py1 link dim' to='/solutions/users'>{strings().header.solutions.users}</Link>
+                            <Link className='block py1 link dim' to='/solutions/nodes'>{strings().header.solutions.nodes}</Link>
+                            <Link className='block py1 link dim' to='/solutions/miners'>{strings().header.solutions.miners}</Link>
+                            <Link className='block py1 link dim' to='/solutions/investors'>{strings().header.solutions.investors}</Link>
                         </div>
 
                         <div className='left-align align-top inline-block footer__section'>
-                            <h4>{ strings().footer.resources }</h4>
-                            <Link className='block py1 link dim' to="/resources/technical">{ strings().footer.technical }</Link>
-                            <Link className='block py1 link dim' to="/resources/non-technical">{ strings().footer.nonTechnical }</Link>
+                            <h4><Link className='white text-decoration-none' to='/technologies'>{strings().header.technologies.title}</Link></h4>
+                            <Link className='block py1 link dim' to='/technologies/adjustable-block-size-cap'>{strings().header.technologies.adjustableCap}</Link>
+                            <Link className='block py1 link dim' to='/technologies/xthin'>{strings().header.technologies.xthin}</Link>
+                            <Link className='block py1 link dim' to='/technologies/parallel-validation'>{strings().header.technologies.parallelValidation}</Link>
                         </div>
 
                         <div className='left-align align-top inline-block footer__section'>
-                            <h4>{ strings().footer.join }</h4>
-                            <a className='block py1 link dim' target='_blank' href='https://www.bitco.in/forum'>{ strings().footer.forum  }</a>
-                            <a className='block py1 link dim' target='_blank' href='https://reddit.com/r/btc'>{ strings().footer.reddit1  }</a>
-                            <a className='block py1 link dim' target='_blank' href='https://reddit.com/r/bitcoin_unlimited'>{ strings().footer.reddit2  }</a>
-                            <a className='block py1 link dim' href='mailto:trevinhofmann@gmail.com?subject=Bitcoin%20Unlimited%20Slack%20Invite&body=Hi%21%0D%0A%0D%0ACould%20you%20please%20invite%20me%20to%20the%20Bitcoin%20Unlimited%20Slack%20group%3F%20My%20email%20address%20is%20%5BINSERT%20EMAIL%20ADDRESS%20HERE%5D.%0D%0A%0D%0AThank%20you%21'>{ strings().footer.slack }</a>
-                            <a className='block py1 link dim' target='_blank' href='https://webchat.freenode.net/?channels=##btc'>{ strings().footer.irc }</a>
+                            <h4><Link className='white text-decoration-none' to='/resources'>{strings().header.resources.title}</Link></h4>
+                            <Link className='block py1 link dim' to='/resources/technical'>{strings().header.resources.technical}</Link>
+                            <Link className='block py1 link dim' to='/resources/non-technical'>{strings().header.resources.nonTechnical}</Link>
                         </div>
 
                         <div className='left-align align-top inline-block footer__section'>
-                            <h4>{ strings().footer.about }</h4>
-                            <Link className='block py1 link dim' to="/about/organization">{ strings().footer.organization }</Link>
-                            <Link className='block py1 link dim' to="/about/members">{ strings().footer.members }</Link>
+                            <h4><Link className='white text-decoration-none' to='/faq'>{strings().header.faq.title}</Link></h4>
+                            <Link className='block py1 link dim' to='/faq/users'>{strings().header.faq.users}</Link>
+                            <Link className='block py1 link dim' to='/faq/nodes'>{strings().header.faq.nodes}</Link>
+                            <Link className='block py1 link dim' to='/faq/miners'>{strings().header.faq.miners}</Link>
+                            <Link className='block py1 link dim' to='/faq/investors'>{strings().header.faq.investors}</Link>
+                        </div>
+
+                        <div className='left-align align-top inline-block footer__section'>
+                            <h4><Link className='white text-decoration-none' to='/about'>{strings().header.about.title}</Link></h4>
+                            <Link className='block py1 link dim' to='/about/organization'>{strings().header.about.organization}</Link>
+                            <Link className='block py1 link dim' to='/about/members'>{strings().header.about.members}</Link>
+                            <Link className='block py1 link dim' to='/about/join'>{strings().header.about.join}</Link>
+                            <Link className='block py1 link dim' to='/about/contact'>{strings().header.about.contact}</Link>
+                        </div>
+
+                        <div className='left-align align-top inline-block footer__section'>
+                            <h4><Link className='white text-decoration-none' to='/download'>{strings().header.download.title}</Link></h4>
+                            <h4><Link className='white text-decoration-none' to='/donate'>{strings().header.donate}</Link></h4>
                         </div>
                     </div>
                     <div className="py3 center">
