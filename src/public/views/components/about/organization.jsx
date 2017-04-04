ff0c4aa5c6e59d7dccd967486bfc3ee00f8d50a5
Sina Habibian <sinahab@gmail.com>
2017-04-04 09:51:49 -0700

    Update 'Organization' section of the About page.

--- src/public/views/components/about/organization.jsx
+++ src/public/views/components/about/organization.jsx
@@ -9,8 +9,18 @@ export default React.createClass({
     render: function () {
         return (
             <div>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus interdum volutpat aliquam. Donec luctus purus vel lectus auctor, eget blandit ante lobortis. Sed non lectus faucibus, auctor augue at, feugiat odio. Ut nisl arcu, pretium faucibus massa nec, finibus lobortis neque. Aliquam vel orci a dolor efficitur placerat. Aenean luctus, ipsum eu feugiat vestibulum, enim elit auctor leo, vitae dapibus arcu tortor nec leo."</p>
-                <p>Our Articles of Federation can be found <a className='link--underline dim black' href="/resources/BUarticles.pdf" target="_blank">here</a>.</p>
+                <p className='lh-copy'>
+                  The Bitcoin Unlimited organization maintains the <a className='link--underline dim black' href="https://github.com/BitcoinUnlimited/BitcoinUnlimited" target="_blank">open-source Bitcoin client</a> by the same name. The organization's mandates are:
+                </p>
+
+                <ol>
+                  <li className='pb1'>To foster competing implementations of the Bitcoin protocol in order to add robustness to the network.</li>
+                  <li>To support the continued growth and adoption of Bitcoin as a peer-to-peer electronic cash system as described in <a className='link--underline dim black' href="https://bitcoin.com/bitcoin.pdf" target="_blank">Satoshi Nakamoto's white paper</a>.</li>
+                </ol>
+
+                <p className='lh-copy'>
+                  The organization is governed by its <a className='link--underline dim black' href="/resources/BUarticles.pdf" target="_blank">Articles of Federation</a> and controversial decisions are settled by members voting. The organization is funded by donations, and it <a className='link--underline dim black' href="https://medium.com/@peter_r/call-for-proposals-making-satoshis-vision-a-reality-with-bitcoin-unlimited-4ff5da246dc7" target="_blank">awards grants</a> to qualified groups and individuals to help fund projects in line with its mandates.
+                </p>
             </div>
 
         );
