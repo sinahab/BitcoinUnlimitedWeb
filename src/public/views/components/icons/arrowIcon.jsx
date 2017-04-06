0c7c678837c661cc8aceeba2761bb496abaf1dd5
Sina Habibian <sinahab@gmail.com>
2017-04-05 18:23:50 -0700

    Fix package versions, remove FillRule, fix React-router

--- src/public/views/components/icons/arrowIcon.jsx
+++ src/public/views/components/icons/arrowIcon.jsx
@@ -6,7 +6,7 @@ class ArrowIcon extends React.Component {
     expandedArrow() {
         return (
             <svg onClick={this.props.onClick} width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5 10L0 0h10" fill="#000" fill-rule="evenodd"/>
+                <path d="M5 10L0 0h10" fill="#000" fillRule="evenodd"/>
             </svg>
         )
     }
@@ -14,7 +14,7 @@ class ArrowIcon extends React.Component {
     unexpandedArrow() {
         return (
             <svg onClick={this.props.onClick} width="10" height="10" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-              <path d="M10 5L0 10V0" fill="#000" fill-rule="evenodd"/>
+              <path d="M10 5L0 10V0" fill="#000" fillRule="evenodd"/>
             </svg>
         )
     }
--- src/public/views/components/icons/bulletIcon.jsx
+++ src/public/views/components/icons/bulletIcon.jsx
@@ -6,7 +6,7 @@ class BulletIcon extends React.Component {
     render() {
         return (
             <svg width={this.props.width} height={this.props.height} viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="4.5" cy="11.5" r="4.5" transform="translate(0 -7)" fill="#000" fill-rule="evenodd"/>
+              <circle cx="4.5" cy="11.5" r="4.5" transform="translate(0 -7)" fill="#000" fillRule="evenodd"/>
             </svg>
         );
     }
