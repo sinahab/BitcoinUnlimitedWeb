3c76e97751949c92fe04bdf5a2646bfb75acff5d
Sina Habibian <sinahab@gmail.com>
2017-04-11 18:33:23 -0700

    Fix react-burger-menu bug by wrapping Radium around Links
    Issue: https://github.com/negomi/react-burger-menu/issues/44

--- src/public/views/components/header/mobileHeader.jsx
+++ src/public/views/components/header/mobileHeader.jsx
@@ -2,8 +2,10 @@
 
 import React from 'react';
 import {Link} from 'react-router';
+import Radium from 'radium';
 import { strings } from '../../../lib/i18n';
 
+const RadiumLink = Radium(Link);
 const Menu = require('react-burger-menu').slide;
 
 class MobileHeader extends React.Component {
@@ -16,22 +18,22 @@ class MobileHeader extends React.Component {
                 </div>
                 <div className="right">
                     <Menu className='center' isOpen={false} right>
-                        <Link className='block my3 dim btn--primary' to='/download'>{strings().header.download.title}</Link>
-                        <Link className='block my3 dim btn--secondary' to='/donate'>{strings().header.donate}</Link>
+                        <RadiumLink className='block my3 dim btn--primary' to='/download'>{strings().header.download.title}</RadiumLink>
+                        <RadiumLink className='block my3 dim btn--secondary' to='/donate'>{strings().header.donate}</RadiumLink>
                         <div className='my3'>
-                            <Link className='white header__link' to='/solutions'>{strings().header.solutions.title}</Link>
+                            <RadiumLink className='white header__link' to='/solutions'>{strings().header.solutions.title}</RadiumLink>
                         </div>
                         <div className='my3'>
-                            <Link className='white header__link' to='/technologies'>{strings().header.technologies.title}</Link>
+                            <RadiumLink className='white header__link' to='/technologies'>{strings().header.technologies.title}</RadiumLink>
                         </div>
                         <div className='my3'>
-                            <Link className='white header__link' to='/resources'>{strings().header.resources.title}</Link>
+                            <RadiumLink className='white header__link' to='/resources'>{strings().header.resources.title}</RadiumLink>
                         </div>
                         <div className='my3'>
-                            <Link className='white header__link' to='/faq'>{strings().header.faq.title}</Link>
+                            <RadiumLink className='white header__link' to='/faq'>{strings().header.faq.title}</RadiumLink>
                         </div>
                         <div className='my3'>
-                            <Link className='white header__link' to='/about'>{strings().header.about.title}</Link>
+                            <RadiumLink className='white header__link' to='/about'>{strings().header.about.title}</RadiumLink>
                         </div>
                     </Menu>
                 </div>
