7feb274077d9c62dd902d1da864a6e7a8c2c2405
Sina Habibian <sinahab@gmail.com>
2017-04-20 13:11:17 -0700

    Use react-modal npm module to display Member info. Remove react-tooltip.
    This was due to two pieces of feedback:
    - when you click on a member's link to pop up his bio, and then click on another member's bio, the previous one is not closed --> they can overlap.
    - The bio’s disappear when one is scrolling up/down. Not much of an issue unless you look at long bios on low-resolution device.

--- src/public/views/components/about/member.jsx
+++ src/public/views/components/about/member.jsx
@@ -2,67 +2,41 @@
 'use strict';
 
 import React from 'react';
-import ReactTooltip from 'react-tooltip'
 
 class Member extends React.Component {
     renderImage() {
-        if (this.props.image) {
-            return (<img className='member__image' src={this.props.image} alt={this.props.name} />)
+        if (this.props.member.photoUrl) {
+            return (<img className='member__image' src={ '../' + this.props.member.photoUrl} alt={this.props.member.name} />)
         } else {
             return
         }
     }
 
     renderTitle() {
-        if (this.props.title) {
-            return ( <div> {this.props.title} </div> )
+        if (this.props.member.title) {
+            return ( <div> {this.props.member.title} </div> )
         } else {
             return
         }
     }
 
     setClickableClass() {
-        if (this.hasTooltip()) {
+        if (this.hasModal()) {
             return('inline-block px0 member--clickable')
         } else {
             return('inline-block px0')
         }
     }
 
-    hasTooltip() {
-        const value = (this.props.publicKey || this.props.bio) ? true : false
+    handleClick() {
+        if (this.hasModal()) {
+            return this.props.modalFunc(this.props.member)
+        }
+    }
+
+    hasModal() {
+        const value = (this.props.member.publicKey || this.props.member.bio) ? true : false
         return value
-    }
-
-    renderTooltipBody() {
-        let bio, name, bitcoin_address = ''
-
-        if (this.props.bio) {
-            bio = this.stringsToParagraphs(this.props.bio)
-            name = <p className='bold center'> {this.props.name} </p>
-        }
-
-        if (this.props.publicKey) {
-            bitcoin_address = "Bitcoin address: " + this.props.publicKey
-        }
-
-        return (
-            <span>
-                <div className='left-align'>
-                    { name }
-                    { bio }
-                </div>
-                <div className='left-align'>
-                    { bitcoin_address }
-                </div>
-            </span>
-        )
-    }
-
-    makeParagraph(string, index) { return <p key={index}>{string}</p> }
-
-    stringsToParagraphs(strings) {
-        return strings.map((string, i) => this.makeParagraph(string, i))
     }
 
     render() {
@@ -71,16 +45,12 @@ class Member extends React.Component {
                 { this.renderImage() }
 
                 <div className='pt1'>
-                    <div> {this.props.name} </div>
-                    <div data-tip data-for={this.hasTooltip() ? this.props.avatar : ''} className={ this.setClickableClass() }>
-                        ({this.props.avatar})
+                    <div onClick={() => this.handleClick()} className={ this.setClickableClass() }>
+                        {this.props.member.avatar}
                     </div>
+                    <div> {this.props.member.name} </div>
                     { this.renderTitle() }
                 </div>
-
-                <ReactTooltip className='member__tooltip' place='bottom' globalEventOff='click' event='click' id={this.props.avatar}>
-                    { this.renderTooltipBody() }
-                </ReactTooltip>
             </div>
         )
     }
--- src/public/views/components/about/members.jsx
+++ src/public/views/components/about/members.jsx
@@ -2,41 +2,67 @@
 'use strict';
 
 import React from 'react';
+import Modal from 'react-modal';
+
 import Member from './member.jsx'
+import MemberModal from './memberModal.jsx'
 
 import MEMBERS from '../../../../data/members.json';
 
 class Members extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            modalIsOpen: false,
+            selectedMember: null
+        };
+
+        this.openMemberModal = this.openMemberModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    openMemberModal(member) {
+        this.setState({
+            modalIsOpen: true,
+            selectedMember: member
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            modalIsOpen: false,
+            selectedMember: null
+        });
+    }
+
+    renderModalBody() {
+        if (this.state.selectedMember) {
+            return (<MemberModal member={this.state.selectedMember} />)
+        }
+    }
+
     render() {
         return (
             <div className='pt3'>
+                <Modal
+                  isOpen={this.state.modalIsOpen}
+                  onRequestClose={this.closeModal}
+                  className='modal--content'
+                  overlayClassName='modal--overlay'
+                  contentLabel="Member Modal"
+                >
+                  {this.renderModalBody()}
+                </Modal>
+
                 <div>
                     {
-                        MEMBERS.withHeadshots.map((member) => {
+                        MEMBERS.map((member) => {
                             return(
                                 <Member
+                                    member={member}
                                     key={member.avatar}
-                                    image={ '../' + member.photoUrl }
-                                    bio={member.bio}
-                                    avatar={member.avatar}
-                                    publicKey={member.publicKey}
-                                    name={member.name}
-                                    title={member.title}
-                                    application={member.link} />
-                            )
-                        })
-                    }
-                </div>
-                <div>
-                    {
-                        MEMBERS.withoutHeadshots.map((member) => {
-                            return(
-                                <Member
-                                    key={member.avatar}
-                                    name={member.name}
-                                    avatar={member.avatar}
-                                    publicKey={member.publicKey}
-                                    application={member.link} />
+                                    modalFunc={this.openMemberModal} />
                             )
                         })
                     }
