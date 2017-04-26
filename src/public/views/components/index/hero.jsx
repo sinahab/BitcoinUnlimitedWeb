dbcdcf77c7783a64c0b6ebf350e1e0a0438ede20
Sina Habibian <sinahab@gmail.com>
2017-04-26 15:59:46 -0400

    Remove homepage slideshow and update copy

--- src/public/views/components/index/hero.jsx
+++ src/public/views/components/index/hero.jsx
@@ -8,28 +8,10 @@ class Hero extends React.Component {
     render() {
         return (
             <div className="hero__container center py3 white">
-                <div className="slideshow">
-                    <section>
-                        <span></span>
-                        <div>
-                            <p className='p2 h1'>{ strings().index.hero.v2.titleOne }</p>
-                        </div>
-                    </section>
-                    <section>
-                        <span></span>
-                        <div>
-                            <p className='p2 h1'>{ strings().index.hero.v2.titleTwo }</p>
-                        </div>
-                    </section>
-                    <section>
-                        <span></span>
-                        <div>
-                            <p className='p2 h1'>{ strings().index.hero.v2.titleThree }</p>
-                        </div>
-                    </section>
-                    <div className='slideshow__image--start'></div>
+                <div className="h1 py4">{strings().index.hero.v1.title}</div>
+                <div className="py3">
+                    <Link className="h3 btn--primary-large hero--button" to='/download'>{strings().index.hero.v1.button}</Link>
                 </div>
-                <Link className="h3 btn--primary-large hero--button" to='/download'>{strings().index.hero.v1.button}</Link>
             </div>
         );
     }
