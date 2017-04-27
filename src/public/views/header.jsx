27df3b332eb722bb8c0de9060e3b40cfa43d6c60
Sina Habibian <sinahab@gmail.com>
2017-04-27 19:11:09 -0400

    Add a hidden security banner in the header which will become visible in case of security announcements, and will be visible on every page.

--- src/public/views/header.jsx
+++ src/public/views/header.jsx
@@ -3,14 +3,30 @@
 import React from 'react';
 import WebHeader from './components/header/webHeader.jsx'
 import MobileHeader from './components/header/mobileHeader.jsx'
+import SecurityBanner from './components/header/securityBanner.jsx'
 
 class Header extends React.Component {
+    renderSecurityMessage() {
+        return
+
+        // Example 1:
+        // return "we have found a bug in abc. We are working to fix it."
+
+        // Example 2:
+        // return (
+        //     <div className='inline'>
+        //         there is an <a href='#' target='_blank' className='orange'>issue</a>.
+        //     </div>
+        // )
+    }
+
     render() {
         return (
             <div>
                 <WebHeader />
                 <MobileHeader />
                 <div className='banner'></div>
+                <SecurityBanner message={ this.renderSecurityMessage() }/>
             </div>
         );
     }
